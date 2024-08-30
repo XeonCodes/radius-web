59bea04e1dae6760ed2759625ba18f534382e505
Oludowole Olumide <xeonncodes@gmail.com>
2024-08-30 14:38:29 +0100

    Updated

--- components/onboard/navbar.tsx
+++ components/onboard/navbar.tsx
@@ -3,16 +3,18 @@ import { Divider } from "@nextui-org/divider";
 import { navbarPadding, title } from "../primitives";
 import { IoCloseSharp } from "react-icons/io5";
 import { MdClose } from "react-icons/md";
+import Link from "next/link";
+import { siteConfig } from "@/config/site";
 
 function Navbar() {
   return (
-    <div className=" sticky top-0 bg-white z-10">
+    <div className="bg-white z-10">
       <nav
         className={`${navbarPadding()} h-24 justify-between flex items-center`}
       >
-        <div>
+        <Link href={`${siteConfig.pathLinks.dashboardHome}`}>
           <h1 className={`text-[28px] text-black font-bold`}>Radius</h1>
-        </div>
+        </Link>
         <div>
           <MdClose color="black" size={24} />
         </div>
